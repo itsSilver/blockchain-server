7ef7f1decbdfc5d9080205f12a3ca53e6754a853
itsSilver <thesilvervevo@gmail.com>
2022-04-09 21:05:53 +0200

    smol changes

--- config/database.js
+++ config/database.js
@@ -5,11 +5,17 @@ module.exports = ({ env }) => ({
       connector: "bookshelf",
       settings: {
         client: "postgres",
-        host: env("DATABASE_HOST", "127.0.0.1"),
+        host: env(
+          "DATABASE_HOST",
+          "ec2-52-48-159-67.eu-west-1.compute.amazonaws.com"
+        ),
         port: env.int("DATABASE_PORT", 5432),
-        database: env("DATABASE_NAME", "blockchain"),
-        username: env("DATABASE_USERNAME", "postgres"),
-        password: env("DATABASE_PASSWORD", "Asroma1927$"),
+        database: env("DATABASE_NAME", "d2fmjeenrnqmn1"),
+        username: env("DATABASE_USERNAME", "hdgenccllriedr"),
+        password: env(
+          "DATABASE_PASSWORD",
+          "6c67576dd8fc9a2935a318fc1b9c2dcaaeb87e7b6af5aca329816861927527e8"
+        ),
         ssl: env.bool("DATABASE_SSL", false),
       },
       options: { strict: "true" },
