48d639255034b276b29033ca35f12bdef88bb20e
itsSilver <thesilvervevo@gmail.com>
2022-04-09 21:18:49 +0200

    smol

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 9000),
+  port: env.int("PORT", 1337),
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET", "f954d4ef8a88b70e22655a854665db3d"),
